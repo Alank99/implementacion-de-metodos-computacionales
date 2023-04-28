b9dceceef68e504c0b738cdc84a046d7fce7ba08
Alank99 <93668758+Alank99@users.noreply.github.com>
2023-04-28 17:04:44 -0600

    correcion

--- elixir/hw/lib/ariel2.ex
+++ elixir/hw/lib/ariel2.ex
@@ -21,50 +21,46 @@ defmodule Hw.Ariel2 do
     """
     def insertion_sort(list), do: do_insertion_sort(list,[])
     defp do_insertion_sort([],sorted), do: sorted
-    defp do_insertion_sort([head | tail],sorted), do: do_insertion_sort(tail, insert(sorted | head))
+    defp do_insertion_sort([head | tail],sorted), 
+        do: do_insertion_sort(tail, insert(sorted , head))
 
+
+    
     @doc """
     rotate a list n times to the left
     """
-    def rotate_left(n, list), do: do_rotate_left(list, n, [])
+    def rotate_left(list, n), do: do_rotate_left(list, n, [])
     
-    defp do_rotate_left([],_n, res), do: res
-    defp do_rotate_left(list, 0, res), do: list ++ res
-    defp do_rotate_left(list, n,res) when n < 0,
+    defp do_rotate_left([],n, res), do: []
+    defp do_rotate_left(list, 0, res), do: list ++ Enum.reverse(res)
+    defp do_rotate_left(list, n, res) when n < 0,
         do: do_rotate_left(list, n+length(list), res)
-    defp do_rotate_left(list, n, rotate) when n > length(list), 
-        do: do_rotate_left(list, n - length(list), rotate)
-    defp do_rotate_left([head | tail], n, res), do: do_rotate_left(tail, n-1,[res | [head]]) 
+    defp do_rotate_left(list, n, res) when n > length(list), 
+        do: do_rotate_left(list, n - length(list), res)
+    defp do_rotate_left([head | tail], n, res), do: do_rotate_left(tail, n-1, [head | res]) 
 
 
     @doc """
     Prime factorization of a number
     """
+
+
     def prime_factors(n), do: do_prime_factors(n,2,[])
-    defp do_prime_factors(1,factor,res), do: res
-    defp do_prime_factors(n,factor,res) when rem(n/factor == 0), 
-        do: do_prime_factors(n/factor,[factor | res])
+    defp do_prime_factors(1,factor,res), do: Enum.reverse(res)
+    defp do_prime_factors(n,factor,res) when rem(n,factor) == 0, 
+        do: do_prime_factors(div(n,factor), factor, [factor | res])
+    defp do_prime_factors(n,factor,res), 
+        do: do_prime_factors(n, factor + 1 , res)
 
-
-
+    
     @doc """
     GCD of two numbers
     """
-    def gcd(a,b), do: do_gcd(a,b,0)
+    def gcd(a,b), do: do_gcd(a,b)
 
-    defp do_gcd(a,0,res), do: res
-    defp do_gcd(a,b,res), do: do_gcd(b, rem(a,b),a)
-
-    def gcd2(a,b), do: do_gcd2(a,b,0)
-    defp do_gcd2(a,0,res), do: res
-    defp do_gcd2(a,b,res), do: do_gcd2(b, a-b,a)
-
-    @doc """deep reverse a list"""
-    def deep reverse(list), do: do_deep_reverse(list,[])
-    defp do_deep_reverse([],res), do: res
-    
-
-
-    
+    defp do_gcd(a,0), do: a
+    defp do_gcd(0,b), do: b
+    defp do_gcd(a,b), do: do_gcd(b, rem(a,b))
 
 end
+ 
