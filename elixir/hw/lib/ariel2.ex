422ab3e3351da83b60c8d6d381a2b4fcee664729
Mario Frias <a01782559@tec.mx>
2023-04-28 20:57:59 -0600

    8-10

--- elixir/hw/lib/ariel2.ex
+++ elixir/hw/lib/ariel2.ex
@@ -1,19 +1,19 @@
 #function to practice recursion in elixir
-#based on the proposats by ariel 
+#based on the proposats by ariel
 #
-#alan hernandez 
+#alan hernandez
 
 defmodule Hw.Ariel2 do
     @doc """
     Insert an element into an ready sorted list
     """
-    
+
     def insert(list,n), do: do_insert(n,list,[])
-    
+
 
     defp do_insert(n,[],res), do: Enum.reverse([n | res])
     defp do_insert(n,[head | tail],res) when n <= head,
-        do: Enum.reverse([n | res]) ++ [head | tail] 
+        do: Enum.reverse([n | res]) ++ [head | tail]
     defp do_insert(n,[head | tail], res), do: do_insert(n,tail,[head | res])
 
     @doc """
@@ -21,23 +21,23 @@ defmodule Hw.Ariel2 do
     """
     def insertion_sort(list), do: do_insertion_sort(list,[])
     defp do_insertion_sort([],sorted), do: sorted
-    defp do_insertion_sort([head | tail],sorted), 
+    defp do_insertion_sort([head | tail],sorted),
         do: do_insertion_sort(tail, insert(sorted , head))
 
 
-    
+
     @doc """
     rotate a list n times to the left
     """
     def rotate_left(list, n), do: do_rotate_left(list, n, [])
-    
+
     defp do_rotate_left([],_n, _res), do: []
     defp do_rotate_left(list, 0, res), do: list ++ Enum.reverse(res)
     defp do_rotate_left(list, n, res) when n < 0,
         do: do_rotate_left(list, n+length(list), res)
-    defp do_rotate_left(list, n, res) when n > length(list), 
+    defp do_rotate_left(list, n, res) when n > length(list),
         do: do_rotate_left(list, n - length(list), res)
-    defp do_rotate_left([head | tail], n, res), do: do_rotate_left(tail, n-1, [head | res]) 
+    defp do_rotate_left([head | tail], n, res), do: do_rotate_left(tail, n-1, [head | res])
 
 
     @doc """
@@ -47,12 +47,12 @@ defmodule Hw.Ariel2 do
 
     def prime_factors(n), do: do_prime_factors(n,2,[])
     defp do_prime_factors(1,_factor,res), do: Enum.reverse(res)
-    defp do_prime_factors(n,factor,res) when rem(n,factor) == 0, 
+    defp do_prime_factors(n,factor,res) when rem(n,factor) == 0,
         do: do_prime_factors(div(n,factor), factor, [factor | res])
-    defp do_prime_factors(n,factor,res), 
+    defp do_prime_factors(n,factor,res),
         do: do_prime_factors(n, factor + 1 , res)
 
-    
+
     @doc """
     GCD of two numbers
     """
@@ -62,28 +62,56 @@ defmodule Hw.Ariel2 do
     defp do_gcd(0,b), do: b
     defp do_gcd(a,b), do: do_gcd(b, rem(a,b))
 
-    
+
+    @doc """
+    8. pack returns a list that groups all the consecutive equal elements of a list
+    """
+    def pack(list), do: do_pack(list, [], [])
+
+    defp do_pack([], [], []), do: []
+    defp do_pack([], new_list, curr), do: Enum.reverse(new_list) ++ [curr]
+    defp do_pack([head | tail], new_list, []), do:
+      do_pack(tail, new_list, [head])
+    defp do_pack([head | tail], new_list, curr) when is_list(curr) do
+      cond do
+        List.first(curr) == head -> do_pack(tail, new_list, [head | curr])
+        true -> do_pack([head | tail], [curr | new_list], [])
+      end
+    end
+    defp do_pack([head | tail], new_list, curr) do
+      cond do
+        curr == head -> do_pack(tail, new_list, [head | curr])
+        true -> do_pack(tail, [[curr] | new_list], head)
+      end
+    end
+
+    @doc """
+    9. compress returns a list that only takes the different values in a sequence
+    """
+    def compress(list), do: do_compress(list, [], [])
+
+    defp do_compress([], [], []), do: []
+    defp do_compress([], new_list, curr), do: Enum.reverse([curr | new_list])
+    defp do_compress([head | tail], new_list, []), do: do_compress(tail, new_list, head)
+    defp do_compress([head | tail], new_list, curr) when head == curr, do:
+      do_compress(tail, new_list, curr)
+    defp do_compress([head | tail], new_list, curr), do:
+      do_compress(tail, [curr | new_list], head)
+
+
     @doc """
     10. encode counts the number of times a value is repeated in a list,
     and returns a list of tuples with the quantity of each different value
     """
-    def encode(list), do: do_encode(list, make_new_list(Enum.uniq(list)))
+    def encode(list), do: do_encode(list, [], [], 0)
 
-    #Crea una nueva lista con listas unicas y con su cantidad en 0
-    defp make_new_list(list), do: List.zip([list, List.duplicate(0, length(list))])
+    defp do_encode([], [], [], 0), do: []
+    defp do_encode([], new_list, curr, count), do: Enum.reverse(new_list) ++ [{count, curr}]
+    defp do_encode([head | tail], new_list, [], 0), do: do_encode(tail, new_list, head, 1)
+    defp do_encode([head | tail], new_list, curr, count) when head == curr, do:
+      do_encode(tail, new_list, curr, count + 1)
+    defp do_encode([head | tail], new_list, curr, count), do:
+      do_encode(tail, [{count, curr} | new_list], head, 1)
 
-    #Remplaza el valor en una lista
-    defp call_replace(list, index, {val, quantity}), do:
-      List.replace_at(list, index, {val, quantity + 1})
-
-    #Recibe el indice y una lista, y devuelve el valor en esa lista
-    defp change_index(list, index), do:
-      call_replace(list, index, Enum.at(list, index))
-
-    defp do_encode([], new_list), do: new_list
-    defp do_encode([head | tail], new_list), do:
-      do_encode(tail,
-        change_index(new_list, Enum.find_index(new_list, fn {v, _q} -> v == head end)))
 
 end
- 
